84439cf88c564806552728f72ff395715af43939
Heikki Wilenius <heikki.wilenius@iki.fi>
2023-09-10 10:54:30 +0300

    linkit päivitetty

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,16 +17,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
+LINKS = (('Suomen antropologi - Journal of the Finnish Anthropological Association', 'https://journal.fi/suomenantropologi/'),
+         ('New regimes of commodification and state formation on the resource frontier of Southeast Asia', 'https://blogs.helsinki.fi/sea-state/'),
          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('@hw@fediscience.org', 'https://fediscience.org/@hw'),
+          ('@suomenantropologi@fediscience.org', 'https://fediscience.org/@suomenantropologi'),)
 
 DEFAULT_PAGINATION = 10
-
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
